bcb7a53bbc1a543eaa4720f5b12a5a37aadc3f6d
rflihmmm <indomilk04@gmail.com>
2024-10-16 13:58:12 +0800

    new repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import { onValue, ref } from "firebase/database"
-import { useEffect, useState } from "react"
-import { db } from "./configs/firebase"
-import CardData from "./components/CardData.jsx"
 import "./App.css"
+import IdCard from "./components/IdCard"
 
 function App() {
-  const [data, setData] = useState([])
-  // const [ph, setPh] = useState([])
-  // const [suhu, setSuhu] = useState([])
-  // const [kk, setKk] = useState([])
-  // const [tds, setTds] = useState([])
-  //
-  useEffect(() => {
-    const query = ref(db, "data/")
-    onValue(query, (snapshot) => {
-      const data = snapshot.val()
-
-      if (snapshot.exists()) {
-        setData(data)
-        // Object.values(data).map((upData) => {
-        //   setUpData((upDatas) => [...upDatas, upData])
-        // })
-      }
-    })
-  }, [])
-
   return (
-    <>
-      <h1 className="mb-4 text-black">Aqua Meter</h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <CardData name="PH" data={data.ph} />
-        <CardData name="Suhu" data={data.suhu} />
-        <CardData name="Kekeruhan" data={data.kk} />
-        <CardData name="TDS" data={data.tds} />
-      </div>
-    </>
+    <div>
+      <IdCard />
+      <button className="btn btn-primary mt-9">Download ID Card</button>
+    </div>
   )
 }
 
